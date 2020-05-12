d1d482b9ce7cd92a6c40ec57e29be9e78089492f
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2020-05-12 17:58:11 +0300

    Update run script

--- run.sh
+++ run.sh
@@ -2,9 +2,4 @@
 set -e
 
 mkdir -p cache
-"${JAVA_HOME}"/bin/java \
-	-Duser.home=cache \
-	-Dsun.awt.noerasebackground=true \
-	-Dcom.jagex.configuri=jagex-jav://oldschool.runescape.com/jav_config.ws \
-	-cp "${JAR:-jagexappletviewer.jar}" jagexappletviewer \
-	"$(basename "${PWD}")"
+clojure -m main
